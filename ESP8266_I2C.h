38744f603b624c5e678d4da1f87ed4994ace9011
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-05-25 23:18:10 +0530

    initial source code files commit

---
+++ ESP8266_I2C.h
@@ -0,0 +1,23 @@
+/****************************************************************
+* ESP8266 I2C LIBRARY'
+*
+* WRAPPER AROUND ESP8266 I2C MASTER DRIVER
+* (ESP8266 DOES NOT HAVE HARDWARE I2C. ESPRESSIF MASTER I2C DRIVER
+  IS SOFTWARE BITBANGING OF I2C PROTOCOL)
+* MAY 25 2017
+*
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+* ------------
+*   (1) TEXAS INSTRUMENTS APP NOTE - UNDERSTANDING I2C
+*       www.ti.com/lit/an/slva704/slva704.pdf
+****************************************************************/
+
+#ifndef _ESP8266_I2C_
+#define _ESP8266_I2C_
+
+
+
+#endif
